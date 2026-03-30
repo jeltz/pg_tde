979f5deb998f0dfe1719737bd84e3d74c65d6688
Andreas Karlsson <andreas.karlsson@percona.com>
2026-03-30 17:03:21 +0200

    WIP

--- src/encryption/enc_tde.c
+++ src/encryption/enc_tde.c
@@ -83,10 +83,12 @@ pg_tde_stream_crypt(const char *iv_prefix,
 					void **ctxPtr)
 {
 	const uint64 aes_start_block = start_offset / AES_BLOCK_SIZE;
-	const uint64 aes_end_block = (start_offset + data_len + (AES_BLOCK_SIZE - 1)) / AES_BLOCK_SIZE;
-	const uint64 aes_block_no = start_offset % AES_BLOCK_SIZE;
+	const uint64 aes_end_block = (start_offset + data_len) / AES_BLOCK_SIZE;
 	uint32		batch_no = 0;
 	uint32		data_index = 0;
+
+	Assert(start_offset % AES_BLOCK_SIZE == 0);
+	Assert(data_len % AES_BLOCK_SIZE == 0);
 
 	/* do max NUM_AES_BLOCKS_IN_BATCH blocks at a time */
 	for (uint64 batch_start_block = aes_start_block; batch_start_block < aes_end_block; batch_start_block += NUM_AES_BLOCKS_IN_BATCH)
@@ -108,33 +110,14 @@ pg_tde_stream_crypt(const char *iv_prefix,
 		}
 #endif
 
-		current_batch_bytes = ((batch_end_block - batch_start_block) * AES_BLOCK_SIZE)
-			- (batch_no > 0 ? 0 : aes_block_no);	/* first batch skips
-													 * `aes_block_no`-th bytes
-													 * of enc_key */
+		current_batch_bytes = (batch_end_block - batch_start_block) * AES_BLOCK_SIZE;
+
 		if ((data_index + current_batch_bytes) > data_len)
 			current_batch_bytes = data_len - data_index;
 
-		for (uint32 i = 0; i < current_batch_bytes; ++i)
+		for (uint32 i = 0; i < current_batch_bytes; i++)
 		{
-			/*
-			 * As the size of enc_key always is a multiple of 16 we start from
-			 * `aes_block_no`-th index of the enc_key[] so N-th will be
-			 * crypted with the same enc_key byte despite what start_offset
-			 * the function was called with. For example start_offset = 10;
-			 * MAX_AES_ENC_BATCH_KEY_SIZE = 6: data:                 [10 11 12
-			 * 13 14 15 16] encKey: [...][0 1 2 3  4  5][0  1  2  3  4  5] so
-			 * the 10th data byte is encoded with the 4th byte of the 2nd
-			 * enc_key etc. We need this shift so each byte will be coded the
-			 * same despite the initial offset. Let's see the same data but
-			 * sent to the func starting from the offset 0: data:    [0 1 2 3
-			 * 4 5 6 7 8 9 10 11 12 13 14 15 16] encKey: [0 1 2 3 4 5][0 1 2 3
-			 * 4 5][ 0 1  2  3  4  5] again, the 10th data byte is encoded
-			 * with the 4th byte of the 2nd enc_key etc.
-			 */
-			uint32		enc_key_index = i + (batch_no > 0 ? 0 : aes_block_no);
-
-			out[data_index] = data[data_index] ^ enc_key[enc_key_index];
+			out[data_index] = data[data_index] ^ enc_key[i];
 
 			data_index++;
 		}
